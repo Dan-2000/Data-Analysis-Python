bebde83851a1b75bce2fc1eb259ca4460d245f08
Dan-2000 <81095977+Dan-2000@users.noreply.github.com>
2025-02-25 23:23:38 +0000

    Gives users the option to search a year to see if Metallica released a song

--- itunes_api_practice.py
+++ itunes_api_practice.py
@@ -1,3 +1,16 @@
+import requests
 base_url = "https://itunes.apple.com/search?"
-r = request.get(base_url, params = {"term": "bring me the horizon", "country": "uk"})
-r.status_code
+r = requests.get(base_url, params = {"term": "metallica", "country": "us", "limit": "200"})
+data = r.json()
+song_release_date= []
+for track in data["results"]:
+        track_name=track.get("trackName")
+        release_Date=track.get("releaseDate")
+        song_release_date.append({"trackName": track_name, "releaseDate": release_Date})
+user_date = input("Please enter the year you would like to check Metalica releases(YYYY):")
+for song in song_release_date:
+    release_year =song["releaseDate"][:4]
+    if(release_year == user_date):
+                print("Track Name:{0} \nRelease Date:{1} \n".format(song["trackName"],song["releaseDate"]))
+    else:
+        print("No songs on the year given. Please enter a different date")
